10ce73bf06d8f92c94c2fccccead5b0944247a01
Mini <truongdangquang1998@gmail.com>
2025-09-05 09:38:37 +0700

    init project

---
+++ user_service/app/api/main.py
@@ -0,0 +1,13 @@
+from fastapi import FastAPI
+from app.core.config import settings
+
+app = FastAPI(title=settings.app_name, version=settings.app_version)
+
+@app.get("/health")
+def health_check():
+    return {
+        "status": "ok",
+        "app": settings.app_name,
+        "version": settings.app_version,
+        "env": settings.app_env,
+    }
